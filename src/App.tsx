f47a3e3223b3f6b98dffa5ee29fe39ea577f6038
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-11-23 00:33:52 +0100

    feat:fazndo as telas e add select do shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 export function App() {
-  return <div>começando projecto</div>;
+  return (
+    <div className="flex">
+      <div className="p-20 space-y-6 bg-gray-700 rounded-lg">
+        <div className="space-y-2">
+          <h1 className="text-2xl text-gray-100 font-bold">
+            Agende um atendimento
+          </h1>
+          <h2 className="text-gray-300 text-sm w-[338px]">
+            Selecione data, horário e informe o nome do cliente para criar o
+            agendamento
+          </h2>
+        </div>
+        <div className="space-y-8">
+          <span className="space-y-2">
+            <h3 className="font-bold text-gray-200">Data</h3>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </span>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
