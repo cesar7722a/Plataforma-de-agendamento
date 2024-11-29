f4cc0fdf5ed66fab96dfc4865d7487a5c10fef1b
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-11-29 23:34:49 +0100

    feat:cofigurando tailwind merge

--- src/components/buttonTime.tsx
+++ src/components/buttonTime.tsx
@@ -1,9 +1,18 @@
 import { Button } from "./ui/button";
 
-export function ButtonTime() {
+interface ButtonTime {
+  children: React.ReactNode;
+  elementRef?: React.LegacyRef<HTMLButtonElement> | undefined;
+  handleClick: () => void;
+}
+export function ButtonTime({ children, elementRef, handleClick }: ButtonTime) {
   return (
-    <Button className="px-5 py-2.5 bg-gray-600 border text-gray-200 border-gray-500 rounded-md hover:cursor-pointer  hover:bg-gray-500">
-      9:00
+    <Button
+      onClick={handleClick}
+      ref={elementRef}
+      className="px-5 py-2.5 bg-gray-600 border text-gray-200 border-gray-500 rounded-md hover:cursor-pointer  hover:bg-gray-500"
+    >
+      {children}
     </Button>
   );
 }
--- src/components/card-time-manha.tsx
+++ src/components/card-time-manha.tsx
@@ -1,13 +1,21 @@
+import { useRef } from "react";
 import { ButtonTime } from "./buttonTime";
 
 export function CardTimeManha() {
+  const buttonRef = useRef<undefined>(null);
+  const handleClick = () => {
+    console.log(buttonRef.current);
+  };
   return (
     <div className="space-y-3">
       <h4 className="text-gray-300 text-sm ">Manhã</h4>
       <div className="grid grid-cols-4 gap-2">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <ButtonTime key={index} />
-        ))}
+        <ButtonTime handleClick={handleClick} elementRef={buttonRef}>
+          9:00
+        </ButtonTime>
+        <ButtonTime handleClick={handleClick}>10:00</ButtonTime>
+        <ButtonTime handleClick={handleClick}>11:00</ButtonTime>
+        <ButtonTime handleClick={handleClick}>12:00</ButtonTime>
       </div>
     </div>
   );
