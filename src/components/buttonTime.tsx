dd399adbc5ed89c856ba3aceb7244143062fb44b
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-12-01 22:44:28 +0100

    feat:add event in button

--- src/components/buttonTime.tsx
+++ src/components/buttonTime.tsx
@@ -1,15 +1,30 @@
+import { ComponentProps } from "react";
 import { Button } from "./ui/button";
 
-interface ButtonTime {
+interface ButtonTime extends ComponentProps<"button"> {
   children: React.ReactNode;
-  elementRef?: React.LegacyRef<HTMLButtonElement> | undefined;
   handleClick: () => void;
+  isDisabled: boolean;
+  key: number;
 }
-export function ButtonTime({ children, elementRef, handleClick }: ButtonTime) {
-  return (
+export function ButtonTime({
+  children,
+  handleClick,
+  isDisabled,
+  key,
+}: ButtonTime) {
+  return isDisabled ? (
     <Button
+      key={key}
       onClick={handleClick}
-      ref={elementRef}
+      className="px-5 py-2.5 bg-gray-600 border text-[#B8952E] border-[#B8952E] rounded-md hover:cursor-pointer  hover:bg-gray-500"
+    >
+      {children}
+    </Button>
+  ) : (
+    <Button
+      key={key}
+      onClick={handleClick}
       className="px-5 py-2.5 bg-gray-600 border text-gray-200 border-gray-500 rounded-md hover:cursor-pointer  hover:bg-gray-500"
     >
       {children}
--- src/components/card-time-manha.tsx
+++ src/components/card-time-manha.tsx
@@ -1,21 +1,41 @@
-import { useRef } from "react";
+import { useState } from "react";
 import { ButtonTime } from "./buttonTime";
 
+interface ButtonProps {
+  title: string;
+  isDisabled: boolean;
+  id: number;
+}
+
 export function CardTimeManha() {
-  const buttonRef = useRef<undefined>(null);
-  const handleClick = () => {
-    console.log(buttonRef.current);
+  const [dataButton, setDataButton] = useState<ButtonProps[]>([
+    { title: "09:00", isDisabled: false, id: 0 },
+    { title: "10:00", isDisabled: false, id: 1 },
+    { title: "11:00", isDisabled: false, id: 2 },
+    { title: "12:00", isDisabled: false, id: 3 },
+  ]);
+  const handleClick = (id: number) => {
+    setDataButton((prev) =>
+      prev.map((item) =>
+        item.id === id && item.isDisabled === false
+          ? { ...item, isDisabled: true }
+          : { ...item, isDisabled: false }
+      )
+    );
   };
   return (
     <div className="space-y-3">
       <h4 className="text-gray-300 text-sm ">Manhã</h4>
       <div className="grid grid-cols-4 gap-2">
-        <ButtonTime handleClick={handleClick} elementRef={buttonRef}>
-          9:00
-        </ButtonTime>
-        <ButtonTime handleClick={handleClick}>10:00</ButtonTime>
-        <ButtonTime handleClick={handleClick}>11:00</ButtonTime>
-        <ButtonTime handleClick={handleClick}>12:00</ButtonTime>
+        {dataButton.map((data) => (
+          <ButtonTime
+            key={data.id}
+            isDisabled={data.isDisabled}
+            handleClick={() => handleClick(data.id)}
+          >
+            {data.title}
+          </ButtonTime>
+        ))}
       </div>
     </div>
   );
