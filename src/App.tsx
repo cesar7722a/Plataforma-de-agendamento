954c4ffd5f09cb4f8154935b42f68f871f4b579a
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-12-06 23:16:16 +0100

    feat:estilizando card sua agenda

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { DatePickerDemo } from "./components/date-picker-demo";
 import { CardTimeManha } from "./components/card-time-manha";
 import { CardTimeTarde } from "./components/card-time-tarde";
 import { CardTimeNoite } from "./components/card-time-noite";
-import { SquareUserRound } from "lucide-react";
+import { CalendarDays, CloudSun, SquareUserRound } from "lucide-react";
 import { Button } from "./components/ui/button";
 import {
   Select,
@@ -61,22 +61,46 @@ export function App() {
           </div>
         </div>
       </div>
-      <div className="py-20 px-28 space-y-2">
-        <div>
-          <span>
-            <h1>Sua agenda</h1>
-            <h2>Consulte os seus cortes de cabelo agendados por dia</h2>
+      <div className="py-20 px-28 space-y-8">
+        <div className="flex w-[590px] justify-between gap-4 items-center">
+          <span className="space-y-1 flex-1">
+            <h1 className="text-4xl font-bold text-gray-100">Sua agenda</h1>
+            <h2 className="text-sm text-gray-300">
+              Consulte os seus cortes de cabelo agendados por dia
+            </h2>
           </span>
           <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Theme" />
+            <SelectTrigger className="w-[170px] flex gap-2 text-gray-200 border border-gray-500 p-3 rounded">
+              <CalendarDays className="size-4 text-[#B8952E]" />
+              <SelectValue placeholder="seleciona data" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">Light</SelectItem>
-              <SelectItem value="dark">Dark</SelectItem>
-              <SelectItem value="system">System</SelectItem>
+              <SelectItem value="1">10/11/2024</SelectItem>
+              <SelectItem value="2">12/03/2024</SelectItem>
+              <SelectItem value="3">01/12/2024</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+        <div className="space-y-3 text-gray-300 text-sm">
+          <div className="rounded border border-gray-600">
+            <div className="flex justify-between px-5 py-3 items-center border border-b-gray-600 border-t-0 border-r-0 border-l-0">
+              <span className="space-x-2 flex items-center">
+                <CloudSun className="size-5 text-[#B8952E]" />
+                <h4>Manhã</h4>
+              </span>
+              <span>9h-12h</span>
+            </div>
+            <ul className="p-5">
+              <li className="py-1 space-x-6 text-gray-200 flex">
+                <span>11:00</span>
+                <h4>Ryan Dorwart</h4>
+              </li>
+              <li className="py-1 space-x-6 text-gray-200 flex">
+                <span>11:00</span>
+                <h4>Ryan Dorwart</h4>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
